fdbf8862f791cfcd86161da904819762e1a0b45a
shiva sharma <shivathebravo@hotmail.com>
2017-07-22 11:55:13 -0400

    Checked Exceptions

---
+++ src/Exceptions/CheckedException.java
@@ -0,0 +1,34 @@
+package Exceptions;
+
+/**
+ * Created by Shiva on 7/22/2017.
+ */
+
+public class CheckedException {
+
+    public static void main(String[] args) {
+
+
+        String reversedString = reverse(null);
+
+        System.out.println(reversedString);
+    }
+
+    private static String reverse(String s) {
+        if (s == null) {
+            try {
+                throw new Exception("The string cannot be null");
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        StringBuilder sb = new StringBuilder(s);
+        System.out.printf("Recversing string %s... \n", s);
+        return sb.reverse().toString();
+
+        // return "This is test";
+    }
+}
+
+
--- src/Exceptions/Exceptions.java
+++ src/Exceptions/Exceptions.java
@@ -8,6 +8,7 @@ public class Exceptions {
     public static void main(String[] args) {
         int i = 12;
         int j = 2;
+
         try {
             int result = i / (j - 2);
             System.out.println(result);
