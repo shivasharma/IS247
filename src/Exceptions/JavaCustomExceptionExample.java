3131cf64380167865f0047f92e2ba1fe365b7165
shiva sharma <shivathebravo@hotmail.com>
2017-07-23 17:56:48 -0400

    JavaCustomException

---
+++ src/Exceptions/JavaCustomExceptionExample.java
@@ -0,0 +1,42 @@
+package Exceptions;
+
+/**
+ * Created by Shiva on 7/23/2017.
+ */
+public class JavaCustomExceptionExample {
+
+    public static void main(String[] args) {        // create a new foo
+        Foo foo = new Foo();
+        // intentionally throw our custom exception by
+        // calling getBar with a zero
+        try {
+            String bar = foo.getBar(10);
+        } catch (AlsCustomException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+/**
+ * Our test class to demonstrate our custom exception.
+ */
+class Foo {
+    public String getBar(int i)
+            throws AlsCustomException {
+        if (i == 0) {
+            throw new AlsCustomException("Anything but zero ...");
+        } else {
+            return "Thanks";
+        }
+
+    }
+}
+
+/**
+ * My custom exception class.
+ */
+class AlsCustomException extends Exception {
+    public AlsCustomException(String message) {
+        super(message);
+    }
+}
