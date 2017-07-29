3906dc7a465ef454e8209dbee30b786e0dd216a5
shiva sharma <shivathebravo@hotmail.com>
2017-07-28 20:48:38 -0400

    Stringbuffer and Stringbuilder

--- src/OOP/StringBuilderEg.java
+++ src/OOP/StringBuilderEg.java
@@ -3,16 +3,18 @@ package OOP;
 /**
  * Created by Shiva on 7/9/2017.
  */
-public class StringBuilder {
+public class StringBuilderEg {
     public static void main(String args[]) {
 
         StringBuffer buffer = new StringBuffer("hello");
         buffer.append("java");
+
+        System.out.println(buffer.append(new StringBuffer()));
         System.out.println(buffer);
 
-        //StringBuilder builder=new StringBuilder("hello");
-        // builder.append("java");
-        //  System.out.println(builder);
+        StringBuilder builder = new StringBuilder("hello");
+        builder.append("java");
+        System.out.println(builder);
     }
 }
     
