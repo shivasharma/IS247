a353b25543e9c341c8e44f23ecab6e4fd95cef8b
shiva sharma <shivathebravo@hotmail.com>
2017-07-29 05:52:03 -0400

    Inheritance

---
+++ src/OOP/Inheritance/Dog.java
@@ -0,0 +1,31 @@
+package OOP.Inheritance;
+
+/**
+ * Created by Shiva on 7/29/2017.
+ */
+class Animal {
+    void eat() {
+        System.out.println("animal is eating...");
+    }
+}
+
+class Dog extends Animal {
+    void eat() {
+        System.out.println("dog is eating...");
+    }
+}
+
+class BabyDog1 extends Dog {
+    public static void main(String args[]) {
+        Animal a = new BabyDog1();
+        a.eat();
+
+        Animal b = new Animal();
+        b.eat();
+
+        Dog d = new Dog();
+        d.eat();
+
+
+    }
+}
