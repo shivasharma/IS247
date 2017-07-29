f3664f87bbe108b01b329e505f648d0072c4a268
shiva sharma <shivathebravo@hotmail.com>
2017-07-29 05:23:08 -0400

    Aggregation

---
+++ src/OOP/CircleAgg.java
@@ -0,0 +1,30 @@
+package OOP;
+
+/**
+ * Created by Shiva on 7/29/2017.
+ */
+
+class Operation {
+    int square(int n) {
+        return n * n;
+    }
+}
+
+public class CircleAgg {
+
+    Operation op;//aggregation
+    double pi = 3.14;
+
+    public static void main(String args[]) {
+        CircleAgg c = new CircleAgg();
+        double result = c.area(10);
+        System.out.println("The area of a square is" + result);
+    }
+
+    double area(int radius) {
+        op = new Operation();
+        int rsquare = op.square(radius);//code reusability (i.e. delegates the method call).
+        return pi * rsquare;
+    }
+
+}
