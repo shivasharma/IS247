423d661403a9565164feead456c47208d52f6226
shiva sharma <shivathebravo@hotmail.com>
2017-08-20 13:55:28 -0400

    Bindings

---
+++ src/OOP/Polymorphim/DynamicBinding.java
@@ -0,0 +1,31 @@
+package OOP.Polymorphim;
+
+/**
+ * Created by Shiva on 8/20/2017.
+ */
+class HumanBeing {
+    public void walk() {
+        System.out.println("Human walks");
+    }
+}
+
+class Boy extends HumanBeing {
+
+    public static void DynaicBinding(String args[]) {
+        /*
+        Here Human is a super class and Boy is a child class since Boy extends Human. Both
+        these classes have a same method void walk(). Since we
+        have assigned the parent class referen to the child
+        class object, during call of walk() method
+        the compiler would not be able to
+        decide which walk() method to call. It will be confused between the walk() method of Human class and walk() method of Boy class.
+        Such kind of bindings are known as dynamic binding as compiler figures out the object type in runtime.
+        */
+        HumanBeing myobj = new Boy();
+        myobj.walk();
+    }
+
+    public void walk() {
+        System.out.println("Boy walks");
+    }
+}
---
+++ src/OOP/Polymorphim/StaticBinding.java
@@ -0,0 +1,27 @@
+package OOP.Polymorphim;
+
+/**
+ * Created by Shiva on 8/20/2017.
+ */
+
+
+class Human {
+}
+
+class Girl extends Human {
+    public void walk() {
+        System.out.println("Girl walks");
+    }
+
+}
+
+
+class StaticBinding {
+    public static void main(String args[]) {
+        // Boy class and calling the method walk() of the same class
+        // Since nothing is ambiguous here, compiler would be able to resolve this binding during compile-time,
+        // such kind of binding is known as static binding.
+        Girl obj1 = new Girl();
+        obj1.walk();
+    }
+}
