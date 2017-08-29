80676b4ba183fb73d042d4a13c89ff1aeaf4424a
shiva sharma <shivathebravo@hotmail.com>
2017-08-28 21:02:46 -0400

    Filewriter

---
+++ src/Exceptions/FileIO/Filewriter.java
@@ -0,0 +1,23 @@
+package Exceptions.FileIO;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by Shiva on 8/28/2017.
+ */
+public class Filewriter {
+
+    public static void main(String[] args) {
+        try {
+            FileWriter writer = new FileWriter("files/MyFile.txt", true);
+            writer.write("Hello World");
+            writer.write("\r\n");   // write new line
+            writer.write("Good Bye!");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
