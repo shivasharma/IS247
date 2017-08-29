f8c1eea8fd85da718765d31a94ca936bd0e7a41f
shiva sharma <shivathebravo@hotmail.com>
2017-08-28 20:25:04 -0400

    File Copy

---
+++ src/Exceptions/FileIO/FilePath.java
@@ -0,0 +1,28 @@
+package Exceptions.FileIO;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+/**
+ * Created by Shiva on 8/28/2017.
+ */
+public class FilePath {
+
+    public static void main(String[] args) {
+
+
+        //This is java7 new library
+        Path sourceFile = Paths.get("files", "target.txt");
+        Path targetFile = Paths.get("files", "traget1.txt");
+
+        try {
+            Files.copy(sourceFile, targetFile, StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("File Copied");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
