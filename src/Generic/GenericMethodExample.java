ddddc6897d41c40816c706b924086622c117a65e
shiva sharma <shivathebravo@hotmail.com>
2017-09-02 17:51:16 -0400

    Added Generic Method

--- src/Generic/GenericMethodExample.java
+++ src/Generic/GenericMethodExample.java
@@ -5,17 +5,16 @@ package Generic;
  */
 public class GenericMethodExample {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         //what will happen if we do that
         //int[] ints={10,20,30,40,50};
-        Integer [] ints={10,20,30,40,50};
-        String[] daysOfWeek= {"Monday","Tuesday" ,"Wednesday","Thursday","Friday","Saturday","Sunday"};
+        Integer[] ints = {10, 20, 30, 40, 50};
+        String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
         print(ints);
         print(daysOfWeek);
-
     }
-    public static<E> void print(E[] list){
-        for(E element:list){
+    public static <E> void print(E[] list) {
+        for (E element : list) {
             System.out.println(element + " ");
         }
         System.out.println(" ");
