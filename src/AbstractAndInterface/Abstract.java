6025aa5cae5c1ebe3c31f654120066e120c11df8
shiva sharma <shivathebravo@hotmail.com>
2017-09-02 00:14:03 -0400

    AbstractClass And Interface

--- src/AbstractAndInterface/Abstract.java
+++ src/AbstractAndInterface/Abstract.java
@@ -3,11 +3,13 @@ package AbstractAndInterface;
 /**
  * Created by Shiva on 9/1/2017.
  */
+//This is abstract class
 abstract class Bike {
     //what happens if the abstract keyword is removed from the method
     abstract void run();
 }
 
+//Honda is a concrete class
 class Honda extends Bike {
     void run() {
         System.out.println("running safely..");
@@ -17,7 +19,12 @@ class Honda extends Bike {
 public class Abstract {
 
     public static void main(String[] args) {
+        //creating an object of Honda class
+        //can we do this
+        //Honda honda= new Bike();
         Bike obj = new Honda();
+
+
         obj.run();
     }
 }
