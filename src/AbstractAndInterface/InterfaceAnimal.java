cc53bd2dab220a3cad92c9f3251e395e2e8980c6
shiva sharma <shivathebravo@hotmail.com>
2017-09-02 00:35:46 -0400

    Interface

---
+++ src/AbstractAndInterface/InterfaceAnimal.java
@@ -0,0 +1,40 @@
+package AbstractAndInterface;
+
+/**
+ * Created by Shiva on 9/2/2017.
+ */
+
+interface Animal {
+    void eat();
+
+    void travel();
+
+    void MamalType();
+}
+
+public class InterfaceAnimal implements Animal {
+
+    public static void main(String args[]) {
+        InterfaceAnimal m = new InterfaceAnimal();
+        m.eat();
+        m.travel();
+        m.MamalType();
+    }
+
+    public void eat() {
+        System.out.println("Mammal eats");
+    }
+
+    public void travel() {
+        System.out.println("Mammal travels");
+    }
+
+    public void MamalType() {
+
+        System.out.println("Tiger");
+    }
+
+    public int noOfLegs() {
+        return 0;
+    }
+}
