718d9faefe1d36f58a71b6b0a0825d17e74fef24
shiva sharma <shivathebravo@hotmail.com>
2017-09-02 00:17:52 -0400

    AbstractClass And Interface

---
+++ src/AbstractAndInterface/AbstractShape.java
@@ -0,0 +1,31 @@
+package AbstractAndInterface;
+
+/**
+ * Created by Shiva on 9/2/2017.
+ */
+abstract class Shape {
+    abstract void draw();
+}
+
+class Rectangle extends Shape {
+    void draw() {
+        System.out.println("drawing rectangle");
+    }
+}
+
+class Circle1 extends Shape {
+    void draw() {
+        System.out.println("drawing circle");
+    }
+}
+
+public class AbstractShape {
+    public static void main(String args[]) {
+
+        Shape rectangel = new Rectangle();
+        Shape shape = new Circle1();
+        shape.draw();
+        rectangel.draw();
+    }
+
+}
