619c438c274ffc01833f2af245493b5c0f5f9ff6
shiva sharma <shivathebravo@hotmail.com>
2017-09-04 11:16:10 -0400

    ArrayList Methods

--- src/Collections/ArrayListMethods.java
+++ src/Collections/ArrayListMethods.java
@@ -34,6 +34,22 @@ public class ArrayListMethods {
         arrli.remove(1);
         System.out.println("Array  after remove method " + arrli);
         System.out.println("The size is " + arrli.size());
+
+
+        //declaring ArrayList with initial size n
+        ArrayList<Integer> arraylist = new ArrayList<Integer>(50);
+        arraylist.add(1);
+        arraylist.add(2);
+        arraylist.add(3);
+        arraylist.add(4);
+        arraylist.add(5);
+        arraylist.add(6);
+        arraylist.add(7);
+        arraylist.add(1);
+        arraylist.add(1);
+        arraylist.add(1);
+        arraylist.trimToSize();
+        System.out.println(arraylist);
     }
 }
 
