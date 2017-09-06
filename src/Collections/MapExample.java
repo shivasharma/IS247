86fb9555ca74620a7d9cd11f1ec9d73fa93fa84f
shiva sharma <shivathebravo@hotmail.com>
2017-09-05 22:09:40 -0400

    Map

--- src/Collections/MapExample.java
+++ src/Collections/MapExample.java
@@ -6,9 +6,9 @@ import java.util.Map;
 public class MapExample {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<Integer, String>();
-        map.put(100, "Amit");
-        map.put(101, "Vijay");
-        map.put(102, "Rahul");
+        map.put(100, "IS247");
+        map.put(101, "IS147");
+        map.put(102, "IS310");
         System.out.println(map);
         for (Map.Entry m : map.entrySet()) {
             System.out.println(m.getKey() + " " + m.getValue());
