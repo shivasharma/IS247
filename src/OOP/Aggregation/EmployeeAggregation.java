68abb9c70932f03c1b179372cad753d27512bf3e
shiva sharma <shivathebravo@hotmail.com>
2017-09-06 22:41:37 -0400

    Aggregation

--- src/OOP/Aggregation/EmployeeAggregation.java
+++ src/OOP/Aggregation/EmployeeAggregation.java
@@ -18,8 +18,8 @@ public class EmployeeAggregation {
     }
 
     public static void main(String[] args) {
-        Address address1 = new Address("gzb", "MD", "USA");
-        Address address2 = new Address("gno", "MT", "CANADA");
+        Address address1 = new Address("Crofton", "MD", "USA");
+        Address address2 = new Address("Baltimore", "MD", "USA");
 
         EmployeeAggregation e = new EmployeeAggregation(111, "John", address1);
         EmployeeAggregation e2 = new EmployeeAggregation(112, "Smith", address2);
