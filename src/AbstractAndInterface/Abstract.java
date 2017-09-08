790cac0e5cdec2cb14e2f0a1f853d4bc108b074e
CIPHER\s.sharma <shivathebravo@gmail.com>
2017-09-08 10:24:49 -0400

    queue

--- src/AbstractAndInterface/Abstract.java
+++ src/AbstractAndInterface/Abstract.java
@@ -4,13 +4,13 @@ package AbstractAndInterface;
  * Created by Shiva on 9/1/2017.
  */
 //This is abstract class
-abstract class Bike {
+abstract class BikeParentClass {
     //what happens if the abstract keyword is removed from the method
     abstract void run();
 }
 
 //Honda is a concrete class
-class Honda extends Bike {
+class HondaChildClass extends BikeParentClass {
     void run() {
         System.out.println("running safely..");
     }
@@ -22,7 +22,7 @@ public class Abstract {
         //creating an object of Honda class
         //can we do this
         //Honda honda= new Bike();
-        Bike obj = new Honda();
+        BikeParentClass obj = new HondaChildClass();
         obj.run();
     }
 }
