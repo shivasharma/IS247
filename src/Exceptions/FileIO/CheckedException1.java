7440a4544440b2aaf3cd66bfa23b8d9abdeccc6a
shiva sharma <shivathebravo@hotmail.com>
2017-09-13 21:00:38 -0400

    Added Unchekced exceptions

---
+++ src/Exceptions/FileIO/CheckedException1.java
@@ -0,0 +1,30 @@
+package Exceptions.FileIO;
+
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by Shiva on 9/13/2017.
+ */
+public class CheckedException1 {
+
+
+   // public static void main(String []args) throws IOException {
+
+    /* try {
+        FileWriter writer = new FileWriter("files/MyFile.txt", true);
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    */
+    public static void main(String []args)  {
+
+           // FileWriter writer = new FileWriter("files/MyFile.txt", true);
+
+
+
+    }
+
+}
