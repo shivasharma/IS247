d1cd9edb72ad676585213f840483e13c51a4dd66
shiva sharma <shivathebravo@hotmail.com>
2017-10-16 22:34:42 -0400

    TowerOfHanoi

--- src/Generic/GenericMethodExample.java
+++ src/Generic/GenericMethodExample.java
@@ -6,15 +6,26 @@ package Generic;
 public class GenericMethodExample {
 
     public static void main(String args[]) {
-        //what will happen if we do that
-        //int[] ints={10,20,30,40,50};
+
         Integer[] ints = {10, 20, 30, 40, 50};
         String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
         print(ints);
         print(daysOfWeek);
+        nonGenericPrint(ints);
     }
+
+    //Generic Method that will print the result based on datatype provided by the users
     public static <E> void print(E[] list) {
         for (E element : list) {
+            System.out.println(element + " ");
+        }
+        System.out.println(" ");
+    }
+
+
+    //This is nonGeneric Print method that take integer array
+    public static void nonGenericPrint(Integer[] list) {
+        for (int element : list) {
             System.out.println(element + " ");
         }
         System.out.println(" ");
