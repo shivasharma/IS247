6f3baee6ffdd0c93a2bd230010de96dafd029355
shiva sharma <shivathebravo@hotmail.com>
2017-10-28 18:35:36 -0400

    Insertion Sort

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -1,42 +1,60 @@
 package Sorting;
 
-import static java.lang.System.currentTimeMillis;
+import java.util.Random;
 
 public class InsertionSort {
 
     public static void main(String a[]) {
-        int[] arr1 = {10, 34, 2, 56, 7, 67, 88};
-        getTime(arr1.length);
-        int[] arr2 = doInsertionSort(arr1);
 
+        //int[] array= {10,34,2,56,7,67,88,42};
+        int[] array;
+
+        array = new int[1000];
+
+        Random rand = new Random();
+        for (int i = 0; i < array.length; i++) {
+            int value = rand.nextInt(1000);
+            System.out.print(" " + value);
+            array[i] = value;
+        }
+
+        insertionTime(array);
+
+        int[] arr2 = insertionSort(array);
         for (int i : arr2) {
             System.out.print(i);
             System.out.print(", ");
         }
     }
 
-    public static int[] doInsertionSort(int[] input) {
 
-        int temp;
-        for (int i = 1; i < input.length; i++) {
-            for (int j = i; j > 0; j--) {
-                if (input[j] < input[j - 1]) {
-                    temp = input[j];
-                    input[j] = input[j - 1];
-                    input[j - 1] = temp;
-                }
+    public static int[] insertionSort(int[] arg) {
+        int key;
+        int i;
+        for (int j = 1; j < arg.length; j++) {
+            key = arg[j];
+            i = j - 1;
+            while (i >= 0 && arg[i] > key) {
+                arg[i + 1] = arg[i];
+                i--;
             }
+            arg[i + 1] = key;
         }
-        return input;
+
+        return arg;
     }
 
-    public static void getTime(long n) {
-        long startTime = currentTimeMillis();
-        long k = 0;
-        for (long i = 1; i <= n; i++) {
-            k = k + 5;
-        }
-        long endTime = currentTimeMillis();
-        System.out.println("Execution Time for n= " + n + " is " + (endTime - startTime) + " milliseconds ");
+
+    public static void insertionTime(int[] arg) {
+        // System.out.println(Arrays.toString(arg));
+        long time1 = System.currentTimeMillis();
+        insertionSort(arg);
+        long time2 = System.currentTimeMillis();
+        long time = time2 - time1;
+        // System.out.println(Arrays.toString(arg));
+        System.out.println("-----------------------");
+        System.out.println("Insertion sort took " + time + " milliseconds.");
     }
+
+
 }
