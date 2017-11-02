92e2367120ffe032d3a9d54783ee64bf5e89b15c
shiva sharma <shivathebravo@hotmail.com>
2017-11-01 22:39:38 -0400

    LinearSearch

--- src/Sorting/QuickSort.java
+++ src/Sorting/QuickSort.java
@@ -13,6 +13,12 @@ public class QuickSort {
             System.out.print(i);
             System.out.print(" ");
         }
+
+        Integer[] ia = {30, 20, 10, 5, 6, 99};
+        GenericQuicksortComparable.qsort(ia, 0, ia.length - 1);
+        for (Integer i : ia) {
+            System.out.println(i);
+        }
     }
 
     public void sort(int[] inputArr) {
@@ -63,5 +69,32 @@ public class QuickSort {
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
+    }
+
+    public static class GenericQuicksortComparable {
+
+        public static <T extends Comparable<T>> void qsort(T[] arr, int a, int b) {
+            if (a < b) {
+                int i = a, j = b;
+                T x = arr[(i + j) / 2];
+
+                do {
+                    while (arr[i].compareTo(x) < 0) i++;
+                    while (x.compareTo(arr[j]) < 0) j--;
+
+                    if (i <= j) {
+                        T tmp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = tmp;
+                        i++;
+                        j--;
+                    }
+
+                } while (i <= j);
+
+                qsort(arr, a, j);
+                qsort(arr, i, b);
+            }
+        }
     }
 }
