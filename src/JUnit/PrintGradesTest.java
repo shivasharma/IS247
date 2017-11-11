aab794e29effe0693c6ef4e3c23c4cc2c938e70a
shiva sharma <shivathebravo@hotmail.com>
2017-11-11 12:46:00 -0500

    JUnit Framework

---
+++ src/JUnit/PrintGradesTest.java
@@ -0,0 +1,15 @@
+package JUnit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PrintGradesTest {
+    @Test
+    public void test() {
+        PrintGrades pg = new PrintGrades();
+        String result = pg.print(95);
+        assertEquals("A", result);
+    }
+
+}
