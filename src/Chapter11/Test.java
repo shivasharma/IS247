ab64beada6602a672e7905ef68672c757f3d7344
ss12@umbc.edu <ss12@umbc.edu>
2023-05-23 12:14:29 -0400

    Added class

--- src/Chapter11/Test.java
+++
@@ -1,5 +0,0 @@
-package Chapter11;
-
-public class Test {
-    //This is test
-}
--- src/OOP/Class.java
+++ src/OOP/Class.java
@@ -6,15 +6,13 @@ package OOP;
 class Class {
 
     void showSum(float v, float x, float y, int count) {
-
         float sum = x + y;
         for (int i = 0; i < count; i++) {
             System.out.println(sum);
         }
-        return;
     }
 
-    void main(String args[]) {
+    void main(String[] args) {
 
         Class c = new Class();
         c.showSum(7.4f, 1, 4, 0);
@@ -31,23 +29,7 @@ class Class {
             BWI3 = BWI1.CreateNewWithBoth(BWI2);
     }
 
-    class Flight {
 
-        private int passengers;
-        private int seat;
-
-        public boolean hasRoom(Flight f2) {
-            int total = passengers + f2.passengers;
-            return total <= seat;
-        }
-
-        public Flight CreateNewWithBoth(Flight f2) {
-            Flight newFlight = new Flight();
-            newFlight.seat = seat;
-            newFlight.passengers = passengers + f2.passengers;
-            return newFlight;
-        }
-    }
 
 }
 
