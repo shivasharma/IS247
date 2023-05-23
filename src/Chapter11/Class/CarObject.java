d88ad9eddb1875e6417044d80d3b05a02e43b49b
ss12@umbc.edu <ss12@umbc.edu>
2023-05-23 14:40:12 -0400

    Added class

---
+++ src/Chapter11/Class/CarObject.java
@@ -0,0 +1,31 @@
+package Chapter11.Class;
+
+// Class definition
+
+
+// Main class
+public class CarObject {
+
+    String brand;
+    String color;
+    int year;
+
+    public static void main(String[] args) {
+        // Creating an object of the Car class
+        CarObject myCar = new CarObject();
+
+        // Setting attribute values
+        myCar.brand = "Toyota";
+        myCar.color = "Red";
+        myCar.year = 2022;
+
+        // Accessing object's attributes and invoking method
+        System.out.println("My car is a " + myCar.year + " " + myCar.brand + " in " + myCar.color + " color.");
+        myCar.drive();
+    }
+
+    // Method
+    void drive() {
+        System.out.println("Driving the " + color + " " + brand + "!");
+    }
+}
