a04ccd60830d621298d50a0fadacda0099fdb3be
ss12@umbc.edu <ss12@umbc.edu>
2023-05-24 16:34:38 -0400

    Added class

--- src/Exceptions/ArrayoutofBoundException.java
+++ src/Exceptions/ArrayoutofBoundException.java
@@ -3,7 +3,6 @@ package Exceptions;
 public class ArrayoutofBoundException {
 
     public static void main(String[] args) {
-
         System.out.println("Element of the array");
         int[] ar = {1, 2, 3};
         for (int i = 0; i <= ar.length; i++) {
---
+++ src/Exceptions/logicalerror.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+public class logicalerror {
+    public static void main(String[] main) {
+        System.out.println(1 / 0);
+
+    }
+}
