3b8fdccfb093d5e33e92d89cfb352c133f76fdae
ss12@umbc.edu <ss12@umbc.edu>
2023-05-24 16:19:22 -0400

    Added class

---
+++ src/Exceptions/StringParser.java
@@ -0,0 +1,10 @@
+package Exceptions;
+
+public class StringParser {
+
+    public static void main(String[] args) {
+        String name = "Shiva Sharma";
+        int i = Integer.parseInt(name);
+        System.out.println(i);
+    }
+}
