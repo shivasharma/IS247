7a1d15c7c6fa28b43486a5f3a30a9e918adf1847
ss12@umbc.edu <ss12@umbc.edu>
2023-05-30 11:43:27 -0400

    Added class

--- src/OOP/Encapsulation/Student.java
+++ src/OOP/Encapsulation/Student.java
@@ -6,6 +6,20 @@ package OOP.Encapsulation;
 public class Student {
     private String name;
 
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age >= 18 && age <= 60) {
+            this.age = age;
+        } else {
+            System.out.println("Please enter the valid age");
+        }
+    }
+
     public String getName() {
         return name;
     }
