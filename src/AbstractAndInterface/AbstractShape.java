11a87945ff024b38c1220dad22693ced88fab764
ss12@umbc.edu <ss12@umbc.edu>
2023-05-30 11:33:19 -0400

    Added class

--- src/AbstractAndInterface/AbstractShape.java
+++ src/AbstractAndInterface/AbstractShape.java
@@ -1,7 +1,11 @@
 package AbstractAndInterface;
 
 /**
- * Created by Shiva on 9/2/2017.
+ * Created by Shiva on 5/30/2023.
+ * //Class Lab
+ * //create a new Square class which extends from Shapr class
+ * //Add draw method and print a message
+ * //Create an object of Square class and display the result
  */
 abstract class Shape {
     abstract void draw();
@@ -19,9 +23,6 @@ class Circle1 extends Shape {
     }
 }
 
-//create a new Square class which extends from Shapr class
-//Add draw method and print a message
-//Create an object of Square class and display the result
 
 public class AbstractShape {
     public static void main(String[] args) {
