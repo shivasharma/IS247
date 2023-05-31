b2bd232d0dd2c19e827eaf566b366a2f772a6a85
ss12@umbc.edu <ss12@umbc.edu>
2023-05-31 11:39:40 -0400

    Added class

---
+++ src/AbstractAndInterface/InterfaceExamples/Student.java
@@ -0,0 +1,14 @@
+package AbstractAndInterface.InterfaceExamples;
+
+public interface Student extends ParentStudent {
+
+    String college = "UMBC";
+
+    void displayName();
+
+    void getStudentNumber();
+
+    void getStandard();
+
+
+}
