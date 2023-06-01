a901e26b1c817a648eb062baaacb9d1f25696274
ss12@umbc.edu <ss12@umbc.edu>
2023-06-01 11:09:34 -0400

    Added class

--- src/Exceptions/Exceptions.java
+++ src/Exceptions/Exceptions.java
@@ -15,11 +15,11 @@ public class Exceptions {
 
 
         } catch (Exception e) {
-            System.out.println(e.fillInStackTrace());
+            //System.out.println(e.fillInStackTrace());
             //System.out.println("Error" + e.getMessage());
             // System.out.println("Error" + e + " cannot divide by zero");
             // This line will show exactly what went wrong
-            // e.printStackTrace();
+            e.printStackTrace();
         }
 
     }
