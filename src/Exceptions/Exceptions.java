8183ca071b24d55d003c6c6ebca41a2b42247d61
ss12@umbc.edu <ss12@umbc.edu>
2023-06-01 11:09:59 -0400

    Added class

--- src/Exceptions/Exceptions.java
+++ src/Exceptions/Exceptions.java
@@ -8,12 +8,9 @@ public class Exceptions {
     public static void main(String[] args) {
         int i = 12;
         int j = 2;
-
         try {
             int result = i / (j - 2);
             System.out.println(result);
-
-
         } catch (Exception e) {
             //System.out.println(e.fillInStackTrace());
             //System.out.println("Error" + e.getMessage());
