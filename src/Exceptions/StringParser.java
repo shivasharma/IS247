42c54dc05bdc339d393cecf382f426cadf38fbd9
ss12@umbc.edu <ss12@umbc.edu>
2023-06-01 10:30:19 -0400

    Added class

--- src/Exceptions/StringParser.java
+++ src/Exceptions/StringParser.java
@@ -5,6 +5,10 @@ public class StringParser {
     public static void main(String[] args) {
         String name = "Shiva Sharma";
         int i = Integer.parseInt(name);
+
         System.out.println(i);
+
+        //Integer.parseInt(name) method returns the integer value which is represented by the argument in decimal equivalent
+        // int i = Integer.parseInt("20"); This code will not throw error
     }
 }
