77066a36b2143077fcadeb69e90e1f3e02ebb6f1
ss12@umbc.edu <ss12@umbc.edu>
2023-06-01 11:33:38 -0400

    Added class

---
+++ src/Exceptions/NotMatchingException.java
@@ -0,0 +1,16 @@
+package Exceptions;
+
+public class NotMatchingException {
+    public static void main(String[] main) {
+        try {
+            System.out.println("Print 1");
+            System.out.println(10 / 0);
+            System.out.println("Print 2");
+        } catch (NullPointerException e) { //ArithmeticException
+            System.out.println("Cannot divide by zero");
+        } finally {
+            System.out.println("What is wrong with this code?");
+        }
+
+    }
+}
