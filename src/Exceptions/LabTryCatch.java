d1a0e625a6d4835f66d51e05eeba6a50ab43fd55
ss12@umbc.edu <ss12@umbc.edu>
2023-06-04 10:45:52 -0400

    Added class

---
+++ src/Exceptions/LabTryCatch.java
@@ -0,0 +1,27 @@
+package Exceptions;
+
+public class LabTryCatch {
+
+    public static void main(String[] args) {
+        StringAppend();
+    }
+
+    public static void StringAppend() {
+        StringBuilder sb = new StringBuilder();
+        /*
+        tried {
+            sb.append("a");
+        catching(Exception ex){
+            sb.append("b");
+        }finale {
+            sb.append("c");
+
+        sb.append("d");
+        System.out.println(sb);
+    }
+
+    }
+       */
+        //The app should print abd
+    }
+}
