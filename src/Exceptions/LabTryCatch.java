42b23fa23b9b09849b3b87a0b3f4e41492c57a77
ss12@umbc.edu <ss12@umbc.edu>
2023-06-04 10:55:08 -0400

    Added class

--- src/Exceptions/LabTryCatch.java
+++ src/Exceptions/LabTryCatch.java
@@ -3,25 +3,42 @@ package Exceptions;
 public class LabTryCatch {
 
     public static void main(String[] args) {
-        StringAppend();
+        // lab1();
+        lab2();
     }
 
-    public static void StringAppend() {
-        StringBuilder sb = new StringBuilder();
-        /*
+    public static void lab1() {
+         /*
+        StringBuilder sb =  StringBuilder();
         tried {
             sb.append("a");
         catching(Exception ex){
             sb.append("b");
         }finale {
             sb.append("c");
-
         sb.append("d");
         System.out.println(sb);
     }
-
     }
        */
         //What is the output of the code?
+    }
+
+    public static void lab2() {
+        //Debug this code?
+        StringBuilder sb = new StringBuilder();
+        String str = null;
+        try {
+            sb.append("a");
+            str.toUpperCase();
+            sb.append("b");
+        } catch (IllegalArgumentException e) {
+            sb.append("c");
+        } catch (Exception e) {
+            sb.append("d");
+        } finally {
+            sb.append("e");
+        }
+        System.out.println(sb);
     }
 }
