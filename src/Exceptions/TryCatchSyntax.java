6e4c03b734a99509290372521f0b9c514aee78b2
ss12@umbc.edu <ss12@umbc.edu>
2023-06-04 10:34:01 -0400

    Added class

---
+++ src/Exceptions/TryCatchSyntax.java
@@ -0,0 +1,33 @@
+package Exceptions;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryCatchSyntax {
+    public static void main(String[] args) {
+
+    }
+
+    public void TrycathSyntax() {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("text.txt"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException ex) {
+            System.err.format("FileNotFoundException %s%n", ex);
+        } catch (IOException ex) {
+            System.err.format("IOException %s%n", ex);
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
