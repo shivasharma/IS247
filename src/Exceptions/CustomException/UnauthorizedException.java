413f4fe0aa4ed4effb35599b7732a6f931785d0c
ss12@umbc.edu <ss12@umbc.edu>
2023-06-05 16:10:34 -0400

    Updated code

---
+++ src/Exceptions/CustomException/UnauthorizedException.java
@@ -0,0 +1,31 @@
+package Exceptions.CustomException;
+
+public class UnauthorizedException extends Exception {
+
+    public UnauthorizedException() {
+    }
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+
+    public static void main(String[] main) {
+        UnauthorizedException un = new UnauthorizedException();
+
+        try {
+            un.login("user", "password");
+        } catch (UnauthorizedException e) {
+            System.out.println("You got and exception" + e);
+        }
+
+    }
+
+    public void login(String userName, String password) throws UnauthorizedException {
+        if (userName.equals("user") && password.equals("pass")) {
+            System.out.println("You can access the application");
+        } else {
+            throw new UnauthorizedException("You do not have permission to hand this error");
+        }
+    }
+
+}
