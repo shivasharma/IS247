fcca9ded3697be047f2f832085c8c67c4f705b9a
ss12@umbc.edu <ss12@umbc.edu>
2023-06-04 23:09:32 -0400

    Updated code

--- src/AbstractAndInterface/AbstractandInterfaceExample.java
+++ src/AbstractAndInterface/AbstractandInterfaceExample.java
@@ -6,6 +6,12 @@ interface InterfaceClass {
     void display(); //all the method are abstract and public
 }
 
+interface InterfaceClass2 {
+    int i = 30; //public static final
+
+    void show(); //all the method are abstract and public
+}
+
 abstract class AbstractClassTest {
     static final int f = 100;
     protected int i = 0;
@@ -13,29 +19,32 @@ abstract class AbstractClassTest {
     abstract void show();
 
     void display() {
-        System.out.println("This is non-abstract method");
+        System.out.println("This is display method from Abstract class");
     }
 }
 
 class AbstractClassExample extends AbstractClassTest {
-
-
     void show() {
         System.out.println("This is a show method from abstract class");
     }
 }
 
-class InterfaceExample implements InterfaceClass {
+class InterfaceExample implements InterfaceClass, InterfaceClass2 {
 
     public void display() {
-        System.out.println("The is a display method from interface I");
+        System.out.println("The is a display method from InterfaceClass ");
     }
 
+    @Override
+    public void show() {
+        System.out.println("This is a show method from InterfaceClass2.");
+    }
 }
 
 public class AbstractandInterfaceExample {
     public static void main(String... args) {
         System.out.println("Example of Abstract Class");
+        System.out.println("*************************************");
         AbstractClassExample a = new AbstractClassExample();
         a.display();
         a.show();
@@ -44,8 +53,12 @@ public class AbstractandInterfaceExample {
         System.out.println(AbstractClassExample.f);
 
         System.out.println("Example of Interface Class");
-        InterfaceClass ref = new InterfaceExample();
+        System.out.println("*************************************");
+        InterfaceExample ref = new InterfaceExample();
+
         ref.display();
+        ref.show();
         System.out.println(InterfaceClass.i);
+
     }
 }
