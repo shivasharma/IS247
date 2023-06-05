d69d84bfcd0559488c05d2e745001deb4e7549ed
ss12@umbc.edu <ss12@umbc.edu>
2023-06-05 10:26:40 -0400

    Updated code

--- src/AbstractAndInterface/InterfaceExamples/SmartPhone.java
+++ src/AbstractAndInterface/InterfaceExamples/SmartPhone.java
@@ -1,5 +1,12 @@
 package AbstractAndInterface.InterfaceExamples;
+/*
+    Create IS247 interface
+    Add a method that return PrintName on IPhone interface();
+    String PrintName(sting name);
+    implement this method which return the name.
+    Print the name from the Main method
 
+ */
 interface Iphone {
     String name = "Iphone";
 
@@ -9,12 +16,10 @@ interface Iphone {
 
 interface Android {
     void RecordVideo();
-
 }
 
 interface BlackBerry {
     void SendMessage();
-
 }
 
 interface Nokia {
@@ -22,9 +27,9 @@ interface Nokia {
 
 }
 
+
 //can you add interface name Windows and implement one of the method  in the SmartPhone class
 public class SmartPhone implements Iphone, Android, BlackBerry, Nokia {
-
 
     public static void main(String[] args) {
         SmartPhone mysmartPhone = new SmartPhone();
@@ -34,22 +39,19 @@ public class SmartPhone implements Iphone, Android, BlackBerry, Nokia {
         mysmartPhone.SendMessage();
     }
 
-    @Override
+
     public void RecordVideo() {
         System.out.println("Andriod Interface Feature");
     }
 
-    @Override
     public void TakePhoto() {
         System.out.println("Iphone Interface Feature");
     }
 
-    @Override
     public void Playgame() {
         System.out.println("Nokia Interface Feature");
     }
 
-    @Override
     public void SendMessage() {
         System.out.println("BlackBerry Interface Feature");
     }
