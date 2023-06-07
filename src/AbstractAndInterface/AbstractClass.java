0859162d488645926cce06ed82c72cd74ae71f9e
ss12@umbc.edu <ss12@umbc.edu>
2023-06-07 11:28:32 -0400

    Junit

--- src/AbstractAndInterface/AbstractClass.java
+++ src/AbstractAndInterface/AbstractClass.java
@@ -4,22 +4,45 @@ package AbstractAndInterface;
  * Created by Shiva on 5/29/2023.
  */
 //This is abstract class
-abstract class BikeParentClass {
+abstract class Vehicle {
     //what happens if the abstract keyword is removed from the method
     abstract void run();
+
     void sleep() {
-        System.out.println("Sleep method from the abstract class BikeParentClass");
+        System.out.println("Sleep method from the abstract class Vehicle");
     }
+
+    abstract String EngineType();
 }
 
 //Honda is a concrete class
-class HondaChildClass extends BikeParentClass {
+class Honda extends Vehicle {
     void run() {
-        System.out.println("Run method from the HondaChildClass extends BikeParentClass");
+        System.out.println("Run method from the Honda extends Vehicle");
+    }
+
+    @Override
+    String EngineType() {
+        String engineType = "Gasoline";
+        return engineType;
     }
 
     void stop() {
-        System.out.println("Stop method from HondaChildClass extends BikeParentClass");
+        System.out.println("Stop method from Honda extends Vehicle");
+    }
+}
+
+class Tesla extends Vehicle {
+
+    @Override
+    void run() {
+
+    }
+
+    @Override
+    String EngineType() {
+        String engineType = "Electric";
+        return engineType;
     }
 }
 
@@ -27,14 +50,13 @@ public class AbstractClass {
 
     public static void main(String[] args) {
         //creating an object of Honda class
-        //can we create object of BikeParentClass
+        //can we create object of Vehicle
         //use honda object and call stop method
-        /*
-        HondaChildClass honda= new BikeParentClass();
-        BikeParentClass obj = new HondaChildClass();
-        obj.run();
-        obj.sleep();
-        */
+
+        Honda honda = new Honda();
+        honda.stop();
+
+
     }
 
 
