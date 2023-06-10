b744ea3b1b15815891798cdd39cfd258116acd08
ss12@umbc.edu <ss12@umbc.edu>
2023-06-09 21:31:36 -0400

    Update file

--- src/FileIO/Filewriter.java
+++ src/FileIO/Filewriter.java
@@ -4,12 +4,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 /**
- * Created by Shiva on 8/28/2017.
+
  */
 public class Filewriter {
 
     public static void main(String[] args) {
         try {
+            //append – boolean if true, then data will be written to the end of the file rather than the beginning.
+            //if append is not defined it will overwrite the file
             FileWriter writer = new FileWriter("files/MyFile.txt", true);
             writer.write("Hello World");
             writer.write("\r\n");   // write new line
