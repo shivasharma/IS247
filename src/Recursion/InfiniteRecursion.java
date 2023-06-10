0290b8b4e6e2becb256601c1c899e346e37220dd
ss12@umbc.edu <ss12@umbc.edu>
2023-06-10 10:07:17 -0400

    Update file

--- src/Recursion/InfiniteRecursion.java
+++ src/Recursion/InfiniteRecursion.java
@@ -5,8 +5,13 @@ public class InfiniteRecursion {
         printIS247();
     }
 
+    //java keeps adding call to the methods (Callstack)
+    //main method call the printIS247 this method goes to stack
     public static void printIS247() {
         System.out.println("IS247");
+        //this again call the printIS247 and add to the stack again
+        //Eventually it will fail with stackoverflow
+        //debug with intellij
         printIS247();
     }
 }
