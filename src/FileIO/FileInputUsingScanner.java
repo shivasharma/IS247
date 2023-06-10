126d3d15259f73aa27bcddcf27e834c6f4be77ac
ss12@umbc.edu <ss12@umbc.edu>
2023-06-09 20:45:24 -0400

    Update file

---
+++ src/FileIO/FileInputUsingScanner.java
@@ -0,0 +1,24 @@
+package FileIO;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileInputUsingScanner {
+    public static void main(String[] args) {
+        Scanner infile;
+        try {
+            //rename the file name scanner1.txt
+            //
+            infile = new Scanner(new File("files/scanner.txt"));
+            int input;
+            while (infile.hasNext()) {
+                input = infile.nextInt();
+                System.out.println(input);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            System.out.println(e.getMessage());
+        }
+    }
+}
