c09275bb71ed26980b0ce9a2d8353590b40bc3de
ss12@umbc.edu <ss12@umbc.edu>
2023-06-11 16:19:34 -0400

    Update file

---
+++ src/Javadocs/Student.java
@@ -0,0 +1,15 @@
+package Javadocs;
+
+/**
+ * Example of Inheritance class
+ */
+public class Student extends Person {
+    /**
+     * This is a construction to initialize person object
+     *
+     * @param name inital person name
+     */
+    public Student(String name) {
+        super(name);
+    }
+}
