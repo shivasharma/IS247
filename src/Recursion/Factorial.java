5cbc893e7175abf122eb670a4557648ed38d8cbb
ss12@umbc.edu <ss12@umbc.edu>
2023-06-13 10:14:41 -0400

    Update file

--- src/Recursion/Factorial.java
+++ src/Recursion/Factorial.java
@@ -19,9 +19,12 @@ public class Factorial {
     }
 
     static double factorial(int n) {
-        if (n == 0)
+        if (n == 0) {
             return 1;
-        else
-            return n * factorial(n - 1);
+        }
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
     }
 }
