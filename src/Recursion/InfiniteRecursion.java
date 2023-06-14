a4c47e40699ebcd4effab4d0063b6fa995738597
ss12@umbc.edu <ss12@umbc.edu>
2023-06-14 17:56:10 -0400

    Update file

--- src/Recursion/InfiniteRecursion.java
+++ src/Recursion/InfiniteRecursion.java
@@ -1,7 +1,9 @@
 package Recursion;
 
 public class InfiniteRecursion {
+    static int i = 0;
     public static void main(String[] args) {
+        int count = 0;
         printIS247();
         //what will happen if we add 1000000 as a value
         // printIS247(5);
@@ -10,7 +12,8 @@ public class InfiniteRecursion {
     //java keeps adding call to the methods (Callstack)
     //main method call the printIS247 this method goes to stack
     public static void printIS247() {
-        System.out.println("IS247");
+        i++;
+        System.out.println(i + " IS247");
         //this again call the printIS247 and add to the stack again
         //Eventually it will fail with stackoverflow
         //debug with intellij
