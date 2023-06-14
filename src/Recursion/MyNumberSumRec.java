76958cc56f15f37ef0cfbdde4fe2856b5547a5fe
ss12@umbc.edu <ss12@umbc.edu>
2023-06-14 11:21:55 -0400

    Update file

--- src/Recursion/MyNumberSumRec.java
+++ src/Recursion/MyNumberSumRec.java
@@ -1,23 +1,14 @@
 package Recursion;
 
+import java.math.BigInteger;
+
 public class MyNumberSumRec {
 
-    int sum = 0;
 
-    public static void main(String a[]) {
-        MyNumberSumRec mns = new MyNumberSumRec();
-        System.out.println("Sum is: " + mns.getNumberSum(1234));
-    }
-
-    public int getNumberSum(int number) {
-
-        if (number == 0) {
-            return sum;
-        } else {
-            int result = number % 10;
-            sum = sum + (number % 10);
-            getNumberSum(number / 10);
-        }
-        return sum;
+    public static BigInteger factorial(long n) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 0; i <= n; i++)
+            result = result.multiply(new BigInteger(i + " "));
+        return result;
     }
 }
