6a06853d892fb0e8042381445b2de51d75252889
ss12@umbc.edu <ss12@umbc.edu>
2023-06-14 10:29:24 -0400

    Update file

--- src/Recursion/Factorial.java
+++ src/Recursion/Factorial.java
@@ -15,7 +15,12 @@ public class Factorial {
 
         int f = in.nextInt();
 
-        System.out.println("Factorial of " + f + " is: " + factorial(f));
+        //  System.out.println("Factorial of " + f + " is: " + factorial(f));
+
+        //System.out.println("Factorial Non Recursive" +factorialNR(f));
+        // System.out.println("Factorial  Recursive " +factorialRec(f));
+        System.out.println("Factorial  Iterative " + iterativeFactorial(f));
+
     }
 
     static int factorial(int n) {
@@ -26,5 +31,38 @@ public class Factorial {
             return 1;
         }
         return n * factorial(n - 1); //Recursive call
+    }
+
+    static int factorialNR(int n) {
+        if (n == 0 || n == 1) { //basecase
+            return 1;
+        }
+        int factorial = n;
+        while (n >= 2) {
+            factorial = factorial * (n - 1);
+            n--;
+        }
+        return factorial;
+
+    }
+
+    static int iterativeFactorial(int n) {
+        int factorial = 1;
+        if (n < 0) {
+            return -1;
+        }
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+        return factorial;
+
+    }
+
+    static int factorialRec(int n) {
+        //this is basecondition what will happend if we remove the if condition
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorialRec(n - 1);
     }
 }
