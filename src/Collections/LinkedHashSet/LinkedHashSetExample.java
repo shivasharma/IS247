f538a9e8f111ab0683873b622f90730cdf284655
ss12@umbc.edu <ss12@umbc.edu>
2023-06-15 10:44:54 -0400

    Update file

---
+++ src/Collections/LinkedHashSet/LinkedHashSetExample.java
@@ -0,0 +1,26 @@
+package Collections.LinkedHashSet;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LinkedHashSetExample {
+    public static void main(String[] args) {
+        CreateLinkedHashSet();
+    }
+
+    private static void CreateLinkedHashSet() {
+        //LinkedHashSet no duplicate
+        // order is maintained
+
+        Set<String> set1 = new LinkedHashSet<>();
+        set1.add("London");
+        //only print single null value
+        set1.add(null);
+        set1.add("Paris");
+        set1.add("New York");
+        set1.add(null);
+        set1.add("New York");
+        set1.add(null);
+        System.out.println(set1);
+    }
+}
