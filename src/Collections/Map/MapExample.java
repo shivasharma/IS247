b095e603361a2e0e3cf65f4b2f5a02ee289e7f61
ss12@umbc.edu <ss12@umbc.edu>
2023-06-16 12:34:52 -0400

    Update file

--- src/Collections/Map/MapExample.java
+++ src/Collections/Map/MapExample.java
@@ -1,20 +1,21 @@
-package Collections;
+package Collections.Map;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MapExample {
     public static void main(String[] args) {
+        CreateMaps();
+
+
+    }
+
+    private static void CreateMaps() {
         Map<Integer, String> map = new HashMap<Integer, String>();
         map.put(100, "IS247");
         map.put(101, "IS147");
         map.put(102, "IS310");
         map.put(102, "IS310");
         System.out.println(map);
-
-
-
-
-
     }
 }
