291583d889eefba8fff54037ebbc588951e639e6
ss12@umbc.edu <ss12@umbc.edu>
2023-06-17 11:45:19 -0400

    Update file

---
+++ src/Generic/TypeInterfaceEg/GenericInterface.java
@@ -0,0 +1,12 @@
+package Generic.TypeInterfaceEg;
+
+public interface GenericInterface<T> {
+
+}
+
+class GenericClass<T> implements GenericInterface<T> {
+
+}
+//class NormalClass implements  GenericInterface<T>{
+
+//}
