91c0bbd4a5b5167e718564ab4c3c7b81eb4e596b
ss12@umbc.edu <ss12@umbc.edu>
2023-06-18 09:00:22 -0400

    Update file

--- src/Generic/TypeInterfaceEg/GenericInterface.java
+++ src/Generic/TypeInterfaceEg/GenericInterface.java
@@ -10,3 +10,28 @@ class GenericClass<T> implements GenericInterface<T> {
 //class NormalClass implements  GenericInterface<T>{
 
 //}
+//Non generic class must specify the specific to implement
+interface Iinterface<T> {
+}
+
+interface Iinterface2 {
+}
+
+//class class0 implements Iinterface<T>{}
+class class0 implements Iinterface<Integer> {
+}
+
+class class1 implements Iinterface<Iinterface2> {
+}
+
+class class2<T> implements Iinterface2 {
+}
+
+class class3<T> extends class2<T> implements Iinterface2 {
+}
+
+/**** Implementing different type *******/
+class class4<T> implements Iinterface<T> {
+}
+//class class5 <T> extends class4<T> implements  Iinterface<Integer>{}
+//class class5 <T> extends class4<T> implements  Iinterface<T>{}
