5f3a02a77858574cb1070b97681b32d46ef17b28
ss12@umbc.edu <ss12@umbc.edu>
2023-06-18 10:25:28 -0400

    Update file

--- src/Generic/BoundedType/BoundClassandInterface.java
+++ src/Generic/BoundedType/BoundClassandInterface.java
@@ -6,17 +6,31 @@ import java.util.List;
 public class BoundClassandInterface {
     public void main(String[] args) {
         List<Integer> listInt = Arrays.asList(12, 55, 7, 9);
-        IBound b = new CBound();
-        getSortedDataBounded(b);
+        IBound1 b = new CBound();
+        getSortedDataSingelBound(b);
+
+        CBound1 b1 = new CBound1();
+        getSortedDataMultipleBound(b1);
+
+
     }
 
-    public <T extends IBound> void getSortedDataBounded(T list) {
+    public <T extends IBound1> void getSortedDataSingelBound(T list) {
     }
 
-    interface IBound {
+    public <T extends IBound1 & IBound2> void getSortedDataMultipleBound(T list) {
     }
 
-    class CBound implements IBound {
+    interface IBound1 {
+    }
+
+    interface IBound2 {
+    }
+
+    class CBound implements IBound1 {
+    }
+
+    class CBound1 implements IBound1, IBound2 {
     }
 }
 
