52b44428d4eed6bc8d0174828949c2d9142df620
ss12@umbc.edu <ss12@umbc.edu>
2023-06-20 09:50:36 -0400

    Update file

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -1,15 +1,28 @@
 package Sorting;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class InsertionSort {
 
-    public static void main(String a[]) {
+    public static void main(String[] a) {
+        //customUnsortedNumber();
+        System.out.println();
+        randomNumberInsertionSort();
 
-        //int[] array= {10,34,2,56,7,67,88,42};
-        int[] array;
+    }
 
-        array = new int[10];
+    private static void customUnsortedNumber() {
+        int[] array = {10, 34, 2, 56, 7, 67, 88, 42, 1, 59};
+        int[] arr2 = insertionSort(array);
+        for (int i : arr2) {
+            System.out.print(i);
+            System.out.print(", ");
+        }
+    }
+
+    private static void randomNumberInsertionSort() {
+        int[] array = new int[10];
 
         Random rand = new Random();
         for (int i = 0; i < array.length; i++) {
@@ -19,12 +32,6 @@ public class InsertionSort {
         }
 
         insertionTime(array);
-
-        int[] arr2 = insertionSort(array);
-        for (int i : arr2) {
-            System.out.print(i);
-            System.out.print(", ");
-        }
     }
 
 
@@ -46,13 +53,16 @@ public class InsertionSort {
 
 
     public static void insertionTime(int[] arg) {
+        System.out.println();
         // System.out.println(Arrays.toString(arg));
         long time1 = System.currentTimeMillis();
         insertionSort(arg);
+
         long time2 = System.currentTimeMillis();
         long time = time2 - time1;
-        // System.out.println(Arrays.toString(arg));
-        System.out.println("-----------------------");
+        System.out.println();
+        System.out.println(Arrays.toString(arg));
+        System.out.println();
         System.out.println("Insertion sort took " + time + " milliseconds.");
     }
 
