61923d3adb502d490a2f22339c3f47fb478c5be0
ss12@umbc.edu <ss12@umbc.edu>
2023-06-21 15:15:24 -0400

    Update file

--- src/Generic/GenericDemo/BankInfo.java
+++ src/Generic/GenericDemo/BankInfo.java
@@ -10,7 +10,17 @@ public class BankInfo<T> {
     public static void main(String[] args) {
         BankInfo<String> b1 = new BankInfo<>("Bank of America");
         BankInfo<Integer> b2 = new BankInfo<>(20);
+        Integer[] m1 = {100, 200, 500};
+        Double[] m2 = {50.5, 78.9, 78.0};
+        b1.deposite(m1);
+        b2.deposite(m2);
 
+    }
+
+    public <T> void deposite(T[] money) {
+        for (T i : money) {
+            System.out.println(i);
+        }
     }
 
     public void display() {
