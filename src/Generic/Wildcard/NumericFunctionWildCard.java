28f002416fc33e351458b2365ad1bc9214b3b0f2
ss12@umbc.edu <ss12@umbc.edu>
2023-06-21 16:21:51 -0400

    Update file

--- src/Generic/Wildcard/NumericFunctionWildCard.java
+++ src/Generic/Wildcard/NumericFunctionWildCard.java
@@ -10,10 +10,16 @@ public class NumericFunctionWildCard<T extends Number> {
     }
 
     public static void main(String[] args) {
+        NumericFunctionWildCard<Integer> num0 = new NumericFunctionWildCard<>(6);
         NumericFunctionWildCard<Integer> num1 = new NumericFunctionWildCard<>(6);
         NumericFunctionWildCard<Double> num2 = new NumericFunctionWildCard<>(-6.0);
-        //num1.absEqualType(num2);
-        num1.absEqualUnknown(num1);
+        NumericFunctionWildCard<Float> num3 = new NumericFunctionWildCard<>(-6.0f);
+        NumericFunctionWildCard<Number> num4 = new NumericFunctionWildCard<>(-6.0f);
+
+        // num1.absEqualType(num1);
+        System.out.println(num1.absEqualUnknown(num2));
+        System.out.println(num1.absEqualUnknown(num3));
+        System.out.println(num1.absEqualUnknown(num4));
     }
 
     boolean absEqualType(NumericFunctionWildCard<T> obj) {
