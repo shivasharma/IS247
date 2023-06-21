577f4cf560c5c359050e80b8d138ee049dc7075f
ss12@umbc.edu <ss12@umbc.edu>
2023-06-21 17:28:20 -0400

    Update file

--- src/Generic/GenericDemo/Data.java
+++ src/Generic/GenericDemo/Data.java
@@ -18,9 +18,15 @@ public class Data {
     }
 
 
+    public static void main(String[] args) {
+        //Data d1=new Data(12);
+        // System.out.println(d1.getData());
+    }
+
     /*
     //What will happend is we replace this with Object datatype
     // How do we handle if we need float/double/integer/String
+    //This is break OCP => Open Close principle
     private Object data;
     public Data(Object data){
         this.data=data;
@@ -38,3 +44,4 @@ public class Data {
         return "data is :" + data;
     }
 }
+
