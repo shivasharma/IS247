c0ea7f956cec265ab26135967306b8a14be52357
ss12@umbc.edu <ss12@umbc.edu>
2023-06-21 14:46:33 -0400

    Update file

--- src/Generic/Wildcard/upperboundwildcardGenerics.java
+++ src/Generic/Wildcard/upperboundwildcardGenerics.java
@@ -1,5 +1,6 @@
 package Generic.Wildcard;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,12 +9,28 @@ public class upperboundwildcardGenerics {
     public static void main(String[] args) {
         List<Integer> li = Arrays.asList(1, 2, 3);
         System.out.println("sum = " + sumOfList(li));
+
+        test(new ArrayList<Integer>());
+        test(new ArrayList<Object>());
+        test2(new ArrayList<Integer>());
+        test2(new ArrayList<Object>());
     }
 
+    //List <? extend Number> means it is bounded and that extends from number only Nu
     public static double sumOfList(List<? extends Number> list) {
         double s = 0.0;
         for (Number n : list)
             s += n.doubleValue();
         return s;
     }
+
+    static void test(ArrayList<?> myList) {
+        System.out.println("This Arraylist<?> method called");
+    }
+
+    static void test2(ArrayList<? extends Object> myList) {
+        System.out.println("This ArrayList<? extends Object method called");
+    }
+
+
 }
