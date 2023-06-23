b8c554ffbd6b3f23d319ecd74f45985c10ff2505
ss12@umbc.edu <ss12@umbc.edu>
2023-06-23 14:39:16 -0400

    Update file

--- src/Sorting/SelectionSort.java
+++ src/Sorting/SelectionSort.java
@@ -2,6 +2,7 @@ package Sorting;
 
 public class SelectionSort {
     public static void selectionSort(int[] arr) {
+        //complexity N*N O(n2)
         for (int i = 0; i < arr.length - 1; i++) {
             int index = i;
             for (int j = i + 1; j < arr.length; j++) {
@@ -15,7 +16,7 @@ public class SelectionSort {
         }
     }
 
-    public static void main(String a[]) {
+    public static void main(String[] a) {
         int[] arr1 = {9, 14, 3, 2, 43, 11, 58, 22};
         System.out.println("Before Selection Sort");
         for (int i : arr1) {
