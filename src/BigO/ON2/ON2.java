35d761ae9d4eeed5b03b7a6c74ef9cbb8282d43a
ss12@umbc.edu <ss12@umbc.edu>
2023-06-23 13:11:33 -0400

    Update file

---
+++ src/BigO/ON2/ON2.java
@@ -0,0 +1,25 @@
+package BigO.ON2;
+
+public class ON2 {
+    /*
+       Complexity is based on  worst case secnario
+       if a single loop of n is O(n), two nested loops will be higher complexity
+       when the value of n changes, each loop takes longer to iterate
+       if n was 1 then it is 1*1  if n is 2 2*2
+       For every iteration of outer loop the inner loop iterate n times
+       n*n
+     */
+    public static void main(String[] args) {
+        nextedForLoop(2);
+    }
+
+    public static void nextedForLoop(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.printf("Product of %s and %s is %s%n", i, j, i * j);
+
+            }
+
+        }
+    }
+}
