98c60a139cb6e98f2f04824d8594912bf11a8c06
ss12@umbc.edu <ss12@umbc.edu>
2023-06-25 10:46:23 -0400

    Update file

--- src/Collections/Map/MapExample.java
+++ src/Collections/Map/MapExample.java
@@ -13,13 +13,37 @@ public class MapExample {
     private static void CreateMaps() {
         Map<Integer, String> map = new HashMap<Integer, String>();
         //insert into the map O(1) complexity
+        //This is not in a sorted order
         map.put(100, "IS247");
         map.put(101, "IS147");
         map.put(102, "IS310");
+        map.put(105, "IS410");
         map.put(102, "IS310");
+
+
         map.put(null, " Null value");
         System.out.println(map);
+
         System.out.println(map.get(null));
         System.out.println(map.get(101));
+
+        System.out.println("----------------------");
+        for (Integer key : map.keySet()) {
+            System.out.println(map.get(key));
+        }
+
+        Printmap(map);
+        //remove item
+        System.out.println("After removing element");
+        map.remove(102);
+        Printmap(map);
+    }
+
+    private static void Printmap(Map<Integer, String> map) {
+        System.out.println("print using entry");
+        System.out.println("----------------------");
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " -" + entry.getValue());
+        }
     }
 }
