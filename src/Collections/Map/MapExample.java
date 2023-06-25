0b601bfea14ec8f7b7563848276b1f57e4982ea2
ss12@umbc.edu <ss12@umbc.edu>
2023-06-25 10:31:49 -0400

    Update file

--- src/Collections/Map/MapExample.java
+++ src/Collections/Map/MapExample.java
@@ -12,10 +12,14 @@ public class MapExample {
 
     private static void CreateMaps() {
         Map<Integer, String> map = new HashMap<Integer, String>();
+        //insert into the map O(1) complexity
         map.put(100, "IS247");
         map.put(101, "IS147");
         map.put(102, "IS310");
         map.put(102, "IS310");
+        map.put(null, " Null value");
         System.out.println(map);
+        System.out.println(map.get(null));
+        System.out.println(map.get(101));
     }
 }
