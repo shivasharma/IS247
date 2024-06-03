cdb867cec0901ee515b622afcb2a10dfcdce54c1
Shiva Sharma <shivathebravo@gmail.com>
2024-06-03 10:39:08 -0400

    updated code

--- src/Exceptions/ExceptionHandling/HandlingFileCheckedException.java
+++ src/Exceptions/ExceptionHandling/HandlingFileCheckedException.java
@@ -1,47 +1,30 @@
 package Exceptions.ExceptionHandling;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.channels.IllegalChannelGroupException;
 public class HandlingFileCheckedException {
     public static void main(String... args) {
 
-        MultipleCatchExamples();
-        //WriteExceptionToLog wel = new WriteExceptionToLog();
-        //FileInput();
-        //FileReader();
-        // doThat();
-        //doThis();
+        //MultipleCatchExamples();
+        FileReader();
     }
-    public static void FileInput() {
-        try {
-            FileInputStream fis = new FileInputStream("Test.txt");
-        } catch (FileNotFoundException e) {
-            // WriteExceptionToLog.writeExceptionToLog(e, e.getMessage());
-            throw new RuntimeException(e);
-        }
-    }
+
     public static void FileReader() {
         try {
-            FileReader fileReader = new FileReader("Test.txt");
-            System.out.println(fileReader.read());
+            FileReader fileReader = new FileReader("files/test.txt");
+            int i;
+            while ((i = fileReader.read()) != -1)
+                System.out.print((char) i);
+            fileReader.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
         }
-    }
 
-    //This is checked exception
-    private static void doThat() throws IOException {
-        throw new IOException();
-    }
-
-    //This is unchecked exceptions
-    private static void doThis() throws IllegalArgumentException {
-        throw new IllegalChannelGroupException();
     }
 
     public static void MultipleCatchExamples() {
