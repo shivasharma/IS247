83f152fb072b1eb9c21785bb63da9b7cb11a7a2a
Shiva Sharma <shivathebravo@gmail.com>
2024-06-03 11:58:11 -0400

    updated code

--- src/Exceptions/CustomException/TestCustomExceptionClass.java
+++ src/Exceptions/CustomException/TestCustomExceptionClass.java
@@ -9,8 +9,8 @@ public class TestCustomExceptionClass {
     private static void ValidateAge(int age) throws AgeLessThenZeroException, CustomExceptionExample {
         if (age < 0) {
             // throw  new CustomExceptionExample("Age cannot be zero");
-            // throw new AgeLessThenZeorException("Invalid age");
-            throw new AgeLessThenZeroException("Error", new RuntimeException());
+            throw new AgeLessThenZeroException("Invalid age");
+            // throw new AgeLessThenZeroException("Error", new RuntimeException());
         }
     }
 }
