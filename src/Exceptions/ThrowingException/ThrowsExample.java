d286ee7637666ed51b3cf1e698e44a25d4b08a2d
Shiva Sharma <shivathebravo@gmail.com>
2024-06-03 11:27:29 -0400

    updated code

---
+++ src/Exceptions/ThrowingException/ThrowsExample.java
@@ -0,0 +1,34 @@
+package Exceptions.ThrowingException;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ThrowsExample {
+    public static void main(String[] args) throws IOException {
+        String filename = "abc.txt";
+        String text = "Hey there, what's up?";
+//        FileWriter fileWriter = new FileWriter(filename);
+//        fileWriter.write(text);
+//        fileWriter.close();
+
+        writeToFile(text, filename);
+        // writeToFileWithTryCatch(text, filename);
+    }
+
+    public static void writeToFile(String text, String filename) throws IOException {
+        FileWriter fileWriter = new FileWriter(filename);
+        fileWriter.write(text);
+        fileWriter.close();
+    }
+
+    public static void writeToFileWithTryCatch(String text, String filename) {
+        try {
+            FileWriter fileWriter = new FileWriter(filename);
+            fileWriter.write(text);
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
