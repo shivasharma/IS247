9ef9d425afaea9e003a32d7cdb4570ea0f5e0ea6
Shiva Sharma <shivathebravo@gmail.com>
2024-06-03 10:14:00 -0400

    updated code

--- src/Exceptions/CheckedException.java
+++ src/Exceptions/CheckedException.java
@@ -2,22 +2,14 @@ package Exceptions;
 
 import java.io.FileReader;
 import java.io.IOException;
-
-/**
- * Created by Shiva on 7/22/2017.
- */
-
 public class CheckedException {
-
     public static void main(String[] args) {
         checkedExpection();
 
     }
-
     private static void checkedExpection() {
-
         try {
-            FileReader fr = new FileReader("files/filereader.txt");
+            FileReader fr = new FileReader("files/filereader1.txt");
             int i;
             while ((i = fr.read()) != -1) {
                 System.out.print((char) i);
@@ -26,7 +18,6 @@ public class CheckedException {
             System.out.println("File does not exist");
 
         }
-        // return reversedString;
     }
 }
 
