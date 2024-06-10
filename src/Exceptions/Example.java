69bd3ae8de7231c8ad704e8d928ca417a360bb01
Shiva Sharma <shivathebravo@gmail.com>
2024-06-10 10:26:43 -0400

    updated code

---
+++ src/Exceptions/Example.java
@@ -0,0 +1,14 @@
+package Exceptions;
+
+public class Example {
+
+    public static void main(String[] args) {
+        System.out.println("Element of the array");
+        int[] ar = {1, 2, 3};
+        for (int i = 0; i <= ar.length; i++) {
+            System.out.println(ar[i]);
+        }
+        //What will happen to this print statement?
+        System.out.println("The above code throw error");
+    }
+}
