822b093e3cbae2008ac43462d05a038583e668a8
Shiva Sharma <shivathebravo@gmail.com>
2024-06-10 09:34:43 -0400

    updated code

--- src/FileIO/FileInputUsingScanner.java
+++ src/FileIO/FileInputUsingScanner.java
@@ -9,11 +9,11 @@ public class FileInputUsingScanner {
         Scanner infile;
         try {
             //rename the file name scanner1.txt
-            infile = new Scanner(new File("files/scanner.txt"));
+            infile = new Scanner(new File("files/scannner.txt"));
             int input;
              //String input;
             while (infile.hasNext()) {
-                //What will happen it the scanner.txt has string values?
+                //What will happen if the scanner.txt has string values?
                 input = infile.nextInt();
                 // input=infile.next();
                 System.out.println(input);
