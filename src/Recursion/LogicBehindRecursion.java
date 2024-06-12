67c099abb318a5aa3b4a5fdb78e83093286e5208
Shiva Sharma <shivathebravo@gmail.com>
2024-06-12 09:30:59 -0400

    Added code

---
+++ src/Recursion/LogicBehindRecursion.java
@@ -0,0 +1,27 @@
+package Recursion;
+
+public class LogicBehindRecursion {
+    public static void main(String[] args) {
+        //LIFO last in first out
+        firstMethod();
+    }
+
+    static void firstMethod() {
+        secondMethod();
+        System.out.println("I am the first method");
+    }
+
+    static void secondMethod() {
+        thirdMethod();
+        System.out.println("I am the second method");
+    }
+
+    static void thirdMethod() {
+        fourthMethod();
+        System.out.println("I am the fourth method");
+    }
+
+    static void fourthMethod() {
+        System.out.println("I am the first method");
+    }
+}
