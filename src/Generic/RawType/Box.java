2d72bbbfc7f7bb63e9b0d44260ea240820edc3b8
Shiva Sharma <shivathebravo@gmail.com>
2024-06-19 11:40:09 -0400

    updated code

---
+++ src/Generic/RawType/Box.java
@@ -0,0 +1,22 @@
+package Generic.RawType;
+
+import java.util.List;
+
+public class Box<T> {
+    private List<T> a;
+
+    public Box(List<T> a) {
+        this.a = a;
+    }
+
+    public List<T> getA() {
+        return a;
+    }
+
+    public void setA(List<T> a) {
+        this.a = a;
+    }
+
+
+}
+
---
+++ src/Generic/RawType/RawTypeDemo.java
@@ -0,0 +1,16 @@
+package Generic.RawType;
+
+import java.util.List;
+
+public class RawTypeDemo {
+    public static void main(String[] args) {
+
+        Box<Integer> genBox = new Box<>(List.of(1, 2, 3));
+        for (Integer integer : genBox.getA()) {
+            System.out.println(integer);
+        }
+        //This is raw type beacuse we did not user <> here
+        Box rawBox = new Box(List.of(1, 2, "test"));
+        System.out.println(rawBox.getA());
+    }
+}
