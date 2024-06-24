07b0c5a2fcee1d79beb8955987434cf6bc3dfa78
Shiva Sharma <shivathebravo@gmail.com>
2024-06-24 13:49:02 -0400

    updated code

--- src/Collections/Map/MapExample2.java
+++ src/Collections/Map/MapExample2.java
@@ -9,7 +9,13 @@ public class MapExample2 {
         marks.put("IS147", 90);
         marks.put("IS247", 96);
         marks.put("IS436", 100); //value is replace key is same
-
         System.out.println(marks);
+
+        System.out.println(marks.get("IS436"));
+        System.out.println(marks.keySet());
+        System.out.println(marks.values());
+        System.out.println(marks.clone());
+        System.out.println(marks.containsKey("IS436"));
+        System.out.println(marks.remove("IS436", 100));
     }
 }
