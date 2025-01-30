29914e73b50789d588b1e347610a36d2bea9da78
Shiva Sharma <ss12@umbc.edu>
2025-01-30 17:16:08 -0500

    Added code

--- src/Chapter11/IS147Recap/Recap.java
+++ src/Chapter11/IS147Recap/Recap.java
@@ -8,32 +8,46 @@ public class Recap {
 
         // Variable declaration and initialization
         int radius = 5;
+
         // Object creation
         Circle circle = new Circle();
+
         // Method call (using method overloading)
-        double area = circle.calculateArea(radius); // Calls the first method
-        double circumference = circle.calculateArea(radius, Math.PI); // Calls the overloaded method
+        // Calls the first method
+        double area = circle.calculateArea(radius);
+
+        // Calls the overloaded method
+
+        double circumference = circle.calculateArea(radius, Math.PI);
         // Output results
         System.out.println("Area of the circle: " + area);
         System.out.println("Circumference of the circle: " + circumference);
+
         // Array declaration and initialization
         int[] numbers = {1, 2, 3, 4, 5};
+
         // Loop (for loop)
         System.out.print("Array elements: ");
         for (int i = 0; i < numbers.length; i++) {
             System.out.print(numbers[i] + " ");
         }
         System.out.println();
+
         // String class usage
         String message = "Hello, Java!";
         System.out.println("Message: " + message);
+
         // Casting (implicit and explicit)
         int intValue = 100;
-        double doubleValue = intValue; // Implicit casting (int to double)
-        int newIntValue = (int) doubleValue; // Explicit casting (double to int)
+        // Implicit casting (int to double)
+        double doubleValue = intValue;
+        // Explicit casting (double to int)
+        int newIntValue = (int) doubleValue;
+
         // Operators (arithmetic and comparison)
         int sum = intValue + newIntValue;
         boolean isEqual = (intValue == newIntValue);
+
         System.out.println("Sum: " + sum);
         System.out.println("Are the values equal? " + isEqual);
     }
