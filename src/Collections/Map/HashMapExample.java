16213b33fada53200b4c7c907bab2c69205b23be
Shiva Sharma <ss12@umbc.edu>
2025-03-31 08:51:17 -0400

    Update HashMapExample.java

--- src/Collections/Map/HashMapExample.java
+++ src/Collections/Map/HashMapExample.java
@@ -22,10 +22,10 @@ public class HashMapExample {
 
         //This will not preserve the order in which these data were inserted
         HashMap<String, String> dictionary = new HashMap<>();
-        dictionary.put("Programming", "Programming refers to a technological process for telling a computer which tasks to perform in order to solve problems.");
-        dictionary.put("Java", "Java is a multiplatform, object-oriented programming language that runs on billions of devices world-wide");
-        dictionary.put("C#", "programming language developed by Microsoft that runs on the .NET Framework");
-        dictionary.put("JavaScript", "JavaScript is the programming language of the Web");
+        dictionary.put("Programming", "Programming refers to a technological process....");
+        dictionary.put("Java", "Java is a multiplatform, object-oriented programming...");
+        dictionary.put("C#", "programming language developed by Microsoft ..");
+        dictionary.put("JavaScript", "JavaScript is the programming ....");
 
         //cannot use for loop
         // for(String word:dictionary){}
@@ -53,10 +53,10 @@ public class HashMapExample {
     private static void LinkedHashMap() {
         //This will preserve the order in which these data were inserted
         LinkedHashMap<String, String> dictionary = new LinkedHashMap<>();
-        dictionary.put("programming", "Programming refers to a technological process for telling a computer which tasks to perform in order to solve problems.");
-        dictionary.put("java", "Java is a multiplatform, object-oriented programming language that runs on billions of devices world-wide");
-        dictionary.put("C#", "programming language developed by Microsoft that runs on the .NET Framework");
-        dictionary.put("JavaScript", "JavaScript is the programming language of the Web");
+        dictionary.put("Programming", "Programming refers to a technological process....");
+        dictionary.put("Java", "Java is a multiplatform, object-oriented programming...");
+        dictionary.put("C#", "programming language developed by Microsoft ..");
+        dictionary.put("JavaScript", "JavaScript is the programming ....");
 
         //cannot use for loop
         // for(String word:dictionary){}
@@ -91,10 +91,10 @@ public class HashMapExample {
     private static void TreeMap() {
         //This will sort in alphabetical order
         TreeMap<String, String> dictionary = new TreeMap<>();
-        dictionary.put("programming", "Programming refers to a technological process for telling a computer which tasks to perform in order to solve problems.");
-        dictionary.put("A", "Java is a multiplatform, object-oriented programming language that runs on billions of devices world-wide");
-        dictionary.put("C#", "programming language developed by Microsoft that runs on the .NET Framework");
-        dictionary.put("E", "JavaScript is the programming language of the Web");
+        dictionary.put("programming", "Programming refers to a ...");
+        dictionary.put("A", "Java is a multiplatform, object-oriented programming...");
+        dictionary.put("C#", "programming language developed by Microsoft ...");
+        dictionary.put("E", "JavaScript is the programming ...");
 
         //cannot use for loop
         // for(String word:dictionary){}
@@ -124,9 +124,9 @@ public class HashMapExample {
     private static void PrintKeyValue() {
         //This will not preserve the order in which these data were inserted
         HashMap<String, String> dictionary = new HashMap<>();
-        dictionary.put("Programming", "Programming refers to a technological process for telling a computer which tasks to perform in order to solve problems.");
-        dictionary.put("Java", "Java is a multiplatform, object-oriented programming language that runs on billions of devices world-wide");
-        dictionary.put("C#", "programming language developed by Microsoft that runs on the .NET Framework");
+        dictionary.put("Programming", "Programming refers to a technological ....");
+        dictionary.put("Java", "Java is a multiplatform, object-oriented ....");
+        dictionary.put("C#", "programming language developed .....k");
         dictionary.put("JavaScript", "JavaScript is the programming language of the Web");
 
         //cannot use for loop
