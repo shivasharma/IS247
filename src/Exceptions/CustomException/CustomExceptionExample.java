c72e3f0b4e0cb6b61939caec40031b44f909b61e
Shiva Sharma <ss12@umbc.edu>
2025-06-03 10:33:44 -0400

    Update CustomExceptionExample.java

--- src/Exceptions/CustomException/CustomExceptionExample.java
+++ src/Exceptions/CustomException/CustomExceptionExample.java
@@ -1,6 +1,6 @@
 package Exceptions.CustomException;
 
-//Extending from the RuntimeException becomes unchecked exception  you do no need to handle it.
+//Extending from the RuntimeException becomes unchecked exception  you do not need to handle it.
 //public class CustomExceptionExample extends RuntimeException{
 public class CustomExceptionExample extends Exception {
     public CustomExceptionExample(String message) {
