994405be3f6cc87d9bced87f0ac9c097610e7295
Shiva Sharma <ss12@umbc.edu>
2025-06-04 13:06:11 -0400

    Update ArrayoutofBoundException.java

--- src/Exceptions/ArrayoutofBoundException.java
+++ src/Exceptions/ArrayoutofBoundException.java
@@ -9,6 +9,6 @@ public class ArrayoutofBoundException {
             System.out.println(ar[i]);
         }
         //What will happen to this print statement?
-        System.out.println("The above code throws error");
+        System.out.println("The above code throws an error");
     }
 }
